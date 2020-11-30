f114b6b89d9f425f1a9fa81f87f21fe5bec6f5b5
bhavana joshi <bhavanajoshi0804@gmail.com>
2020-11-30 19:49:01 +0530

    fix

--- push.js
+++ push.js
@@ -7,6 +7,15 @@ let vapidkeys = {
   
   push.setVapidDetails("mailto:someone@example.com",vapidkeys.publicKey, vapidkeys.privateKey)
   
-  let sub = {};
+  let sub = {
+      endpoint:
+      "https://fcm.googleapis.com/fcm/send/cotx3x1aHKM:APA91bFsakNblXRHdrQvs03pZj7EPvYP3G9MXwaAhnwDyYWALF2Hjv2ySD8xjnAGi1lwg_uLf1n4R5W-Eofb-dQ5ddz0Voxn0E_O4kOdeXZYsRH5juwieT3-ewStQ2_zZaplWhEywRAu",
+      expirationTime:null,
+      keys:{
+          p256dh:
+          "BCP0qDxhrI-dj6_6pWwAo5RswEiX5kEPchVI7d-d5ihOgQhFiZ9xQm2Z6m4lIDpnXVfgoYE8WErPajgovVivkhg",
+          auth:"c_mQ7UjwEdWWDO-tu6wuqg"
+        }
+    };
   
   push.sendNotification(sub, 'test message')
